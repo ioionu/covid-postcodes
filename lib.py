13a3f04796b8ad059505c691bc885fb44154e080
josh <io@ioio.nu>
2021-10-03 19:04:28 +1100

    master switch to eb

--- lib.py
+++ lib.py
@@ -3,8 +3,10 @@ import os
 
 def get_connection():
     return psycopg2.connect(
-        dbname=os.environ['DATABASE_NAME'],
-        user=os.environ['DATABASE_USER'],
-        password=os.environ['DATABASE_PASSWORD'],
-        host=os.environ['DATABASE_HOST']
+        dbname=os.environ['RDS_DB_NAME'],
+        user=os.environ['RDS_USERNAME'],
+        password=os.environ['RDS_PASSWORD'],
+        host=os.environ['RDS_HOSTNAME'],
+        port=os.environ['RDS_PORT']
     )
+
